f2d93bf3a49a070f0815a9d491c061ceb49226a2
KANG JIHYO <luxuryjihyo@naver.com>
2024-08-20 19:46:08 +0900

    first commit

---
+++ src/main/java/com/example/travewebportal/TravelWebPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.travewebportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelWebPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelWebPortalApplication.class, args);
+    }
+
+}
