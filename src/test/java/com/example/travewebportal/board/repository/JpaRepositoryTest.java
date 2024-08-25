4145b3ce7464594d1ab6da33fa625a6b51e79928
KANG JIHYO <luxuryjihyo@naver.com>
2024-08-25 13:44:59 +0900

    test

---
+++ src/test/java/com/example/travewebportal/board/repository/JpaRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.example.travewebportal.board.repository;
+
+import com.example.travewebportal.board.config.JpaConfig;
+import com.example.travewebportal.board.domain.Article;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("JPA Connection Test")
+@Import(JpaConfig.class) //auditing 설정 넣어줘야함
+@DataJpaTest
+class JpaRepositoryTest {
+
+
+    private final ArticleRepository articleRepository;
+
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,@Autowired ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("Select Test")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine(){
+        // Given
+
+        // When
+        List<Article> articles = articleRepository.findAll();
+        // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(300);
+    }
+
+}
