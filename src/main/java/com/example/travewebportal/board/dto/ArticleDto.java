9c002510ade70ae5a5a1a381c74aaacf57433f5a
KANG JIHYO <luxuryjihyo@naver.com>
2024-09-05 09:20:15 +0900

    add dto

---
+++ src/main/java/com/example/travewebportal/board/dto/ArticleDto.java
@@ -0,0 +1,17 @@
+package com.example.travewebportal.board.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public record ArticleDto (
+        LocalDateTime createdAt,
+        String createdBy,
+        String title,
+        String content,
+
+        String hashTag) implements Serializable {
+
+    public static ArticleDto of(LocalDateTime createdAt, String createdBy, String title, String content, String hashTag) {
+        return new ArticleDto(createdAt,createdBy,title,content, hashTag);
+    }
+}
