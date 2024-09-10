04ad9c4e74574d48467cad082183098e4f492eef
KANG JIHYO <luxuryjihyo@naver.com>
2024-09-10 22:58:10 +0900

    add  article comment dto & service

---
+++ src/main/java/com/example/travewebportal/board/ArticleCommentService.java
@@ -0,0 +1,28 @@
+package com.example.travewebportal.board;
+
+import com.example.travewebportal.board.dto.ArticleCommentDto;
+import com.example.travewebportal.board.dto.ArticleDto;
+import com.example.travewebportal.board.dto.ArticleUpdateDto;
+import com.example.travewebportal.board.enums.SearchType;
+import com.example.travewebportal.board.repository.ArticleCommentRepository;
+import com.example.travewebportal.board.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleCommentRepository articleCommentRepository;
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+        return List.of();
+    }
+}
