849b17efb6373623a50b11e432ceccad7ad27770
KANG JIHYO <luxuryjihyo@naver.com>
2024-09-16 12:36:32 +0900

    feat dto

--- src/main/java/com/example/travewebportal/board/dto/ArticleUpdateDto.java
+++ src/main/java/com/example/travewebportal/board/dto/ArticleUpdateDto.java
@@ -1,5 +1,11 @@
 package com.example.travewebportal.board.dto;
 
+import com.example.travewebportal.board.domain.UserAccount;
+import lombok.Value;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
 public record ArticleUpdateDto(
         String title,
         String content,
@@ -8,5 +14,22 @@ public record ArticleUpdateDto(
 
     public static ArticleUpdateDto of(String title, String content, String hashTag) {
         return new ArticleUpdateDto(title,content, hashTag);
+    }
+
+    /**
+     * DTO for {@link UserAccount}
+     */
+    @Value
+    public static class UserAccountDto implements Serializable {
+        LocalDateTime createdAt;
+        String createdBy;
+        LocalDateTime modifiedAt;
+        String modifiedBy;
+        Long id;
+        String userId;
+        String password;
+        String email;
+        String nickname;
+        String memo;
     }
 }
--- src/main/java/com/example/travewebportal/board/dto/UserAccountDto.java
+++ src/main/java/com/example/travewebportal/board/dto/UserAccountDto.java
@@ -1,5 +1,6 @@
 package com.example.travewebportal.board.dto;
 
+import com.example.travewebportal.board.domain.UserAccount;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -28,5 +29,30 @@ public record UserAccountDto(
                                     LocalDateTime modifiedAt,
                                     String modifiedBy) {
         return new UserAccountDto(id,userId,password,email,nickname,memo,createdAt,createdBy,modifiedAt, modifiedBy);
+    }
+
+    public static UserAccountDto from(UserAccount entity){
+        return new UserAccountDto(
+                entity.getId(),
+                entity.getUserId(),
+                entity.getPassword(),
+                entity.getEmail(),
+                entity.getNickname(),
+                entity.getMemo(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public UserAccount toEntity(){
+        return UserAccount.of(
+                userId,
+                password,
+                email,
+                nickname,
+                memo
+        );
     }
 }
