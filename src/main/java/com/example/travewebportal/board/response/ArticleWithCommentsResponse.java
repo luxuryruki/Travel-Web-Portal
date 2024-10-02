6d3f122fd60a92870f71132b4cf225786f5709e4
KANG JIHYO <luxuryjihyo@naver.com>
2024-10-02 10:26:42 +0900

    Response

---
+++ src/main/java/com/example/travewebportal/board/response/ArticleWithCommentsResponse.java
@@ -0,0 +1,73 @@
+package com.example.travewebportal.board.response;
+
+import com.example.travewebportal.board.dto.ArticleCommentDto;
+import com.example.travewebportal.board.dto.ArticleDto;
+import com.example.travewebportal.board.dto.ArticleWithCommentsDto;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public record ArticleWithCommentsResponse(
+        Long id,
+        String title,
+        String content,
+        String hashtag,
+        LocalDateTime createdAt,
+        String email,
+        String nickname,
+        String userId,
+        Set<ArticleCommentResponse> articleCommentsResponse
+) {
+
+    public static ArticleWithCommentsResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, String userId, Set<ArticleCommentResponse> articleCommentResponses) {
+        return new ArticleWithCommentsResponse(id, title, content, hashtag, createdAt, email, nickname, userId, articleCommentResponses);
+    }
+
+    public static ArticleWithCommentsResponse from(ArticleWithCommentsDto dto) {
+        String nickname = dto.userAccountDto().nickname();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.userAccountDto().userId();
+        }
+
+        return new ArticleWithCommentsResponse(
+                dto.id(),
+                dto.title(),
+                dto.content(),
+                dto.hashTag(),
+                dto.createdAt(),
+                dto.userAccountDto().email(),
+                nickname,
+                dto.userAccountDto().userId(),
+                organizeChildComments(dto.articleCommentDtos())
+        );
+    }
+
+
+    private static Set<ArticleCommentResponse> organizeChildComments(Set<ArticleCommentDto> dtos) {
+        Map<Long, ArticleCommentResponse> map = dtos.stream()
+                .map(ArticleCommentResponse::from)
+                .collect(Collectors.toMap(ArticleCommentResponse::id, Function.identity()));
+
+        map.values().stream()
+                .filter(ArticleCommentResponse::hasParentComment)
+                .forEach(comment -> {
+                    ArticleCommentResponse parentComment = map.get(comment.parentCommentId());
+//                    parentComment.childComments().add(comment);
+                });
+
+        return map.values().stream()
+                .filter(comment -> !comment.hasParentComment())
+                .collect(Collectors.toCollection(() ->
+                        new TreeSet<>(Comparator
+                                .comparing(ArticleCommentResponse::createdAt)
+                                .reversed()
+                                .thenComparingLong(ArticleCommentResponse::id)
+                        )
+                ));
+    }
+}
